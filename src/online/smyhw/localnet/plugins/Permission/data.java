e0feee5ee7dda04bbefa51079cf84801471e349d
smyhw <i@smyhw.online>
2020-06-13 19:24:26 +0800

    edit

--- src/online/smyhw/localnet/plugins/Permission/data.java
+++ src/online/smyhw/localnet/plugins/Permission/data.java
@@ -36,13 +36,13 @@ public class data
 					{//读取组权限内容
 						String temp2 = reader.readLine();
 						if(temp2==null || !temp2.startsWith("-")){break;}
-						re.add(temp2.substring(1));//分离开头的"-"
+						re.add(temp2.substring(1));
 					}
 					break;
 					//这里直接返回
 				}
 			}
-			
+			return getPermissionList(re);//解释
 		}
 		catch (Exception e) 
 		{
@@ -62,6 +62,38 @@ public class data
 		{
 			message.warning("读取权限文件出错", e);
 		}
+		return re;
+	}
+	
+	
+	public static List<String> getPermissionList(List<String> PermissionString)
+	{
+		List<String> re = new ArrayList<String>();
+		for(String temp1 : PermissionString)
+		{
+			if(temp1.contains(":"))
+			{//解释特殊语句
+				String[] temp2 = temp1.split(":");
+				switch(temp2[0])
+				{
+				case "group"://继承其他权限组的全部权限
+				{
+					
+				}
+				continue;
+				case "player"://继承其他玩家的全部权限
+				{
+					
+				}
+				continue;
+				default://未知则不处理
+					continue;
+				}
+			}
+			//如果到这还没有continue，则表示这是个正常的权限节点文本
+			re.add(temp1);
+		}
+		//遍历完每一行后，返回
 		return re;
 	}
 }
